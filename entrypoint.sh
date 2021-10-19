11d03e89dc1812c016087515da0ceb9f371bd4f1
Curtis Ruck <curtis@ruck.io>
2021-10-19 14:34:43 -0400

    add a psql with sample query

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,10 @@ done
 
 until pg_isready; do
 	echo "Waiting for pg_isready"
+	sleep 1
 done
-sleep 5
+
+until psql -qa -c 'select 1 connected;'; do
+	echo "Waiting for successful connection"
+	sleep 1
+done
